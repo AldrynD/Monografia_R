34de7e0220253c9a09903254e540848f1d806fba
Aldryn <aldryn.dylan@yahoo.com>
2022-10-23 16:31:12 -0300

    script_att

--- database_script.R
+++ database_script.R
@@ -42,6 +42,21 @@ database <- data %>%
 
 
 
+teste <- data %>% 
+        mutate(cred = case_when(
+                data$exp_ipca <= data$inflacao_meta_central ~ 1,
+                data$exp_ipca > data$inflacao_meta_central & 
+                        data$exp_ipca < 20 ~ (1-(1/(20-data$inflacao_meta_central))*(data$exp_ipca - data$inflacao_meta_central)),
+                data$exp_ipca >= 20 ~ 0
+                )
+        ) %>%
+        select(periodo, cred)
+
+ggplot(data = teste) + aes(x = periodo, y = cred) + geom_line() + scale_y_continuous(limits=c(0, 1))
+
+
+
+
 
 
 
